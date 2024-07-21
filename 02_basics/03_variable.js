2b45e75baa00f916363c5bab14fd35c47aad0688
anaya0314 <razaalichamp.0314@gmail.com>
2024-07-21 14:13:12 +0000

    variable

---
+++ 02_basics/03_variable.js
@@ -0,0 +1,11 @@
+const accountId = 2356484
+let accountEmail = "ali.anaya@gmail.com"
+var accountPassword = "12345"
+let accountState;
+console.log(accountId);
+
+accountEmail =" shah@hotmail.com"
+accountPassword = "321321"
+accountCity = "karachi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 02_basics/test.js
@@ -0,0 +1,2 @@
+console.log("sarim")
+console.log("ali")
